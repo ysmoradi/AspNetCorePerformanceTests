7ab4c87f3e6671e8dda32fb59645b5fd4477972e
ysmoradi <ysmoradi@outlook.com>
2017-12-09 20:35:12 +0330

    Initial program.cs & app startup.cs

--- AspNetCorePerformanceTests/Startup.cs
+++ AspNetCorePerformanceTests/Startup.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCorePerformanceTests
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            RouteBuilder routeBuilder = new RouteBuilder(app);
 
-            app.Run(async (context) =>
+            routeBuilder.MapGet("api/values/{id}", context =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                return context.Response.WriteAsync("value");
             });
+
+            IRouter routes = routeBuilder.Build();
+
+            app.UseRouter(routes);
         }
     }
 }
