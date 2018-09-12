67c76ae8c2f51ad5680d24259e786efaecdcf4d0
Yaser Moradi <ysmoradi@outlook.com>
2018-09-12 12:43:59 +0430

    Updated nodejs sample

--- node/index.js
+++ node/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+﻿const express = require('express');
+const app = express();
 
 app.get('/api/values/:id/', (req, res) => {
-    res.send(req.params.id)
+    res.send(req.params.id);
 });
 
-app.listen(5000, () => console.log('Example app listening on port 3000!'));
+app.listen(5000, () => console.log('Example app listening on port 5000!'));
